3bec59b88c218a0fbb0f2f8182b7a77eb769b438
Lion <stegerl@in.tum.de>
2021-08-05 11:23:11 +0200

    Adding mountains
    Adding blue background
    Adding several layers of trees and mountains
    Making makimum recursion depth argument for trees
    Reworking output

--- fractaltree.py
+++ fractaltree.py
@@ -28,6 +28,11 @@ ANGLE = np.pi / 2  # angle between branches in the same level
 PERTURB = 5.0  # perturb the angle a little to make the tree look random
 RATIO = 0.8  # contraction factor between successive trunks
 
+PEAK_COUNT = 16
+MOUNTAIN_DEPTH = 18
+MOUNTAIN_HEIGHT = 1000
+MOUNTAIN_BASE_COLOR = 0.6
+
 TRUNK_BOOST = 1.5  # boosting factor of trunk length
 GRASS_ANGLE_SD = 0.3  # grass angle standard deviation
 
@@ -53,9 +58,10 @@ if args.mode == "debug_grass":
 
 ROOT = (WIDTH / 2.0, HEIGHT)
 
+
 # Return an interpolation of the two colors `ROOT_COLOR` and `leaf_color`
-def get_color(level, leaf_color):
-    a = float(level) / ITERATIONS
+def get_color(level, max_iter, leaf_color):
+    a = float(level) / max_iter
     return a * ROOT_COLOR + (1 - a) * leaf_color
 
 
@@ -65,16 +71,16 @@ def get_random_leaf_color():
 
 
 # Return the line width of a given level.
-def get_line_width(level, trunk_rad):
-    return max(1, (trunk_rad / ((ITERATIONS - level) / 2 + 1)))
+def get_line_width(level, max_iter, trunk_rad):
+    return max(1, (trunk_rad / ((max_iter - level) / 2 + 1)))
 
 
 # Return a random branch length depending on level and angle
-def get_random_branch_length(level, angle, t):
-    randt  = min(1, (np.random.random() + (level / (ITERATIONS * 5))))
+def get_random_branch_length(level, max_iter, angle, t):
+    randt  = min(1, (np.random.random() + (level / (max_iter * 5))))
     randt *= 1 / (1 + np.abs(np.pi / 2 - angle) / 6)
     randt *= t
-    randt *= TRUNK_BOOST if level == ITERATIONS else 1
+    randt *= TRUNK_BOOST if level == max_iter else 1
     return randt
 
 
@@ -90,6 +96,29 @@ def get_random_grass_length(length_init, angle):
 # Return a random grass color
 def get_random_grass_color():
     return np.array([GRASS_COLOR[i] + (np.random.random() - 0.5) * GRASS_COLOR_WEIGHTS[i] for i in range(3)])
+
+
+# Generate and draw a mountain
+def mountains(ctx, height, base, root, base_color, level):
+    if level <= 0:
+        return
+
+    rootx, rooty = root
+
+    ctx.set_line_width(0)
+    color  = base_color * (0.93 ** (MOUNTAIN_DEPTH - level))
+    color *= ((HEIGHT - rooty) / HEIGHT - 0.45) * 2 + 2
+    ctx.set_source_rgb(color, color, color)
+    ctx.move_to(rootx - base, rooty)
+    ctx.line_to(rootx, rooty - height)
+    ctx.line_to(rootx + base, rooty)
+    ctx.line_to(rootx - base, rooty)
+    ctx.fill()
+
+    if np.random.random() < (level / MOUNTAIN_DEPTH) * 1.15: mountains(ctx, height // 2, base // 2, (rootx + base // 2, rooty), base_color, level - 1)
+    if np.random.random() < (level / MOUNTAIN_DEPTH) * 1.15: mountains(ctx, height // 2, base // 2, (rootx - base // 2, rooty), base_color, level - 1)
+    if np.random.random() < (level / MOUNTAIN_DEPTH) * 0.95: mountains(ctx, height // 2, base // 2, (rootx, rooty - height // 2), base_color, level - 1)
+    if np.random.random() < (level / MOUNTAIN_DEPTH) * 0.95: mountains(ctx, height // -2, base // 2, (rootx, rooty - height // 2), base_color, level - 1)
 
 
 # Generate and draw a bunch of grass
@@ -117,19 +146,18 @@ def grass(ctx, root, width_init, length_init):
 
 
 # Generate and draw a fractal tree
-def fractal_tree(ctx, level, start, t, leaf_color, trunk_rad, r, theta, angle, perturb):
+def fractal_tree(ctx, max_iter, level, start, t, leaf_color, trunk_rad, r, theta, angle, perturb):
     if level == 0:
         return
 
     x0, y0 = start
-
-    randt = get_random_branch_length(level, theta, t)
+    randt = get_random_branch_length(level, max_iter, theta, t)
     x, y = x0 + randt * np.cos(theta), y0 - randt * np.sin(theta)
 
-    color = get_color(level, leaf_color)
+    color = get_color(level, max_iter, leaf_color)
     ctx.move_to(x0, y0)
     ctx.line_to(x, y)
-    ctx.set_line_width(get_line_width(level, trunk_rad))
+    ctx.set_line_width(get_line_width(level, max_iter, trunk_rad))
     ctx.set_source_rgb(*color)
     ctx.stroke()
 
@@ -141,10 +169,30 @@ def fractal_tree(ctx, level, start, t, leaf_color, trunk_rad, r, theta, angle, p
         theta2 = max((-np.pi / 8), min((9 * np.pi / 8), theta2))
 
     # recursively draw the next branches
-    fractal_tree(ctx, level - 1, (x, y), t * r, leaf_color, trunk_rad,
+    fractal_tree(ctx, max_iter, level - 1, (x, y), t * r, leaf_color, trunk_rad,
                  r, theta1, angle, perturb)
-    fractal_tree(ctx, level - 1, (x, y), t * r, leaf_color, trunk_rad,
+    fractal_tree(ctx, max_iter, level - 1, (x, y), t * r, leaf_color, trunk_rad,
                  r, theta2, angle, perturb)
+
+
+# Wrapper function to kick off recursive tree generation
+def fractal_tree_outer(ctx, iters, rad_init, len_init):
+    root_x = (np.random.random() - 0.5) * (WIDTH - 100)
+    root = (ROOT[0] + root_x, ROOT[1])
+    leaf_color = get_random_leaf_color()
+    trunk_rad = rad_init + ((np.random.random() - 0.5) * (rad_init / 4))
+    trunk_len = len_init + ((np.random.random() - 0.5) * (len_init / 4))
+    fractal_tree(ctx, iters, iters, root, trunk_len, leaf_color, trunk_rad,
+                 RATIO, THETA, ANGLE, PERTURB)
+
+
+# Wrapper function to kick off recursive mountain generation
+def mountain_outer(ctx, base_color):
+    base = np.random.normal(loc=MOUNTAIN_HEIGHT, scale=400)
+    height = np.random.normal(loc=base, scale=10)
+    root_x = (np.random.random() - 0.5) * (WIDTH + 500)
+    root = (ROOT[0] + root_x, ROOT[1])
+    mountains(ctx, height, base, root, base_color, MOUNTAIN_DEPTH)
 
 
 def main():
@@ -152,25 +200,30 @@ def main():
     ctx = cairo.Context(surface)
     ctx.set_line_cap(cairo.LINE_CAP_ROUND)
     ctx.set_line_join(cairo.LINE_JOIN_ROUND)
-    ctx.set_source_rgb(1, 1, 1)
+    ctx.set_source_rgb(0.5, 0.75, 1)
     ctx.paint()
 
-    for i in range(TREE_COUNT):
-        sys.stdout.write(f"{i} / {TREE_COUNT} \r")
-        root_x = (np.random.random() - 0.5) * (WIDTH - 50)
-        root = (ROOT[0] + root_x, ROOT[1])
-        leaf_color = get_random_leaf_color()
-        trunk_rad = TRUNK_RAD + ((np.random.random() - 0.5) * (TRUNK_RAD / 4))
-        trunk_len = TRUNK_LEN + ((np.random.random() - 0.5) * (TRUNK_LEN / 4))
-        fractal_tree(ctx, ITERATIONS, root, trunk_len, leaf_color, trunk_rad,
-                     RATIO, THETA, ANGLE, PERTURB)
+    for fac in [1, 2][::-1]:
+        for i in range(PEAK_COUNT):
+            sys.stdout.write(f"{i + 1} / {PEAK_COUNT} \r")
+            mountain_outer(ctx, MOUNTAIN_BASE_COLOR / fac)
 
-    print("\nTrees done")
+        print(f"\nMountains factor {fac} done.")
+
+    for fac in [1, 1.5, 2, 2.5, 3, 4][::-1]:
+        for i in range(TREE_COUNT):
+            sys.stdout.write(f"{i + 1} / {TREE_COUNT} \r")
+            fractal_tree_outer(ctx, ITERATIONS // np.sqrt(fac), TRUNK_RAD / fac, TRUNK_LEN / fac)
+
+        print(f"\nTrees factor {fac} done.")
 
     for i in range(GRASS_LOWER_COUNT):
+        sys.stdout.write(f"{i + 1} / {GRASS_LOWER_COUNT} \r")
         root_x = (np.random.random() - 0.5) * (WIDTH - 5)
         root = (ROOT[0] + root_x, ROOT[1])
         grass(ctx, root, GRASS_RAD, GRASS_LEN)
+
+    print("\nGrass done")
 
     surface.write_to_png("random_fractal_tree.png")
 
